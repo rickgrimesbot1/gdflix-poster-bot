18cf603c57986146a98e4955c7204264709ee61c
Rick Grimes <160530437+rickgrimesbot1@users.noreply.github.com>
2026-01-09 18:53:22 +0530

    Delete rick directory

--- rick/__init__.py
+++
@@ -1,8 +0,0 @@
-# package marker + centralized logger
-import logging
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-logger = logging.getLogger("bot")
